22b3772f346a769082a02d8ae1cbe9c020192b7f
Artyom Sheldyaev <cmashinho@gmail.com>
2018-03-16 20:14:33 +0300

    Added custom colors

---
+++ Docker Client/Docker Client/UIUtils.swift
@@ -0,0 +1,23 @@
+//
+//  UIUtils.swift
+//  Docker Client
+//
+//  Created by Artyom Sheldyaev on 16.03.2018.
+//  Copyright © 2018 Artyom Sheldyaev. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class var appleRed: UIColor {
+        return UIColor(red:1.00, green:0.23, blue:0.19, alpha:1.0)
+    }
+    
+    class var appleYellow: UIColor {
+        return UIColor(red:1.00, green:0.80, blue:0.00, alpha:1.0)
+    }
+    
+    class var appleGreen: UIColor {
+        return UIColor(red:0.30, green:0.85, blue:0.39, alpha:1.0)
+    }
+}
