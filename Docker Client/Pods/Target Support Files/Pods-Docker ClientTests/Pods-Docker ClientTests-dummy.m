8026417947db131894e0e35d2e4eab1d49979bf0
Artyom Sheldyaev <cmashinho@gmail.com>
2018-04-17 16:42:57 +0300

    Added podfile and install just

---
+++ Docker Client/Pods/Target Support Files/Pods-Docker ClientTests/Pods-Docker ClientTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Docker_ClientTests : NSObject
+@end
+@implementation PodsDummy_Pods_Docker_ClientTests
+@end
---
+++ Docker Client/Pods/Target Support Files/Pods-Docker ClientUITests/Pods-Docker ClientUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Docker_ClientUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Docker_ClientUITests
+@end
