ceb6d521cbd7e0a1b57140e65b33b34dd08a466b
Artyom Sheldyaev <cmashinho@gmail.com>
2018-04-17 17:16:26 +0300

    Added SwiftyJSON and initial commit

---
+++ Docker Client/Docker Client/RequestManager+ResponseHandler.swift
@@ -0,0 +1,20 @@
+//
+//  RequestManager+ResponseHandler.swift
+//  Docker Client
+//
+//  Created by Artyom Sheldyaev on 17.04.2018.
+//  Copyright © 2018 Artyom Sheldyaev. All rights reserved.
+//
+
+enum RequestType {
+    
+    func path() -> {
+        
+    }
+}
+
+extension RequestManager {
+    func dataReceived() {
+        
+    }
+}
---
+++ Docker Client/Docker Client/RequestManager.swift
@@ -0,0 +1,21 @@
+//
+//  RequestManager.swift
+//  Docker Client
+//
+//  Created by Artyom Sheldyaev on 17.04.2018.
+//  Copyright © 2018 Artyom Sheldyaev. All rights reserved.
+//
+
+import Just
+
+class RequestManager {
+    static let shared = RequestManager()
+    
+    func _baseGET(_ RequestType: ) {
+        
+    }
+    
+    func _basePOST() {
+        
+    }
+}
